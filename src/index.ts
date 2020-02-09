6e05c14d834594f491f72bd6f2f9c0c1bb1fc975
Joseph Lust <lifeoflust@gmail.com>
2020-02-09 18:00:03 -0500

    Updates

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,6 @@ async function run(): Promise<void> {
   const context = github.context;
   const event = context.payload;
 
-  core.warning(`Using github keys [${JSON.stringify(Object.keys(github))}]`);
-  // core.warning(`Using github.context [${JSON.stringify(context)}]`);
-
   if(projectId) {
     setVariable(EnvironmentVariables.CI_PROJECT_ID, projectId);
   }
@@ -17,9 +14,9 @@ async function run(): Promise<void> {
   setVariable(EnvironmentVariables.CI, 'true');
   setVariable(EnvironmentVariables.CI_NAME, 'github');
 
-  // @ts-ignore - missing lib defs
-  const buildId = github?.run_id;
-  setVariable(EnvironmentVariables.CI_BUILD_ID, buildId);
+  // TODO get from toolkit - isn't available in that object presently
+  setVariable(EnvironmentVariables.CI_BUILD_ID, '${{ github.run_id }}');
+  setVariable(EnvironmentVariables.CI_BUILD_APPROVED, 'false');
 
   setVariable(EnvironmentVariables.CI_COMMIT_ID, context?.sha);
 
@@ -30,10 +27,17 @@ async function run(): Promise<void> {
   setVariable(EnvironmentVariables.CI_COMMITTER_EMAIL, author?.email);
   setVariable(EnvironmentVariables.CI_COMMITTER_NAME, author?.name);
 
+  // TODO add expected description (rather complex to dig up)
+  // Expected description format - "2019-07-18.1563481767-1-g7f60" < 'g<first 4 sha char>'
+  //       commit date branched from ^     same sec ^     ^ commit number on branch (0 is the commit you branched from)
+  setVariable(EnvironmentVariables.CI_COMMIT_DESCRIPTION, '');
+
   const nowMilliseconds = Date.now();
-  const nowAsTimeString = new Date(nowMilliseconds).toISOString();
-  setVariable(EnvironmentVariables.CI_TIMESTAMP, nowMilliseconds.toString());// TODO test in CS, confirm expected format
-  setVariable(EnvironmentVariables.CI_STRING_TIME, nowAsTimeString);// TODO test in CS, confirm expected format
+  const nowSeconds = Math.floor(nowMilliseconds/1000);
+  const nowAsTimeIsoString = new Date(nowMilliseconds).toISOString();
+  const nowAsTimeExpectedIsoString = nowAsTimeIsoString.replace(/\.\d{3}Z/,'Z'); // No millis expect
+  setVariable(EnvironmentVariables.CI_TIMESTAMP, nowSeconds.toString());
+  setVariable(EnvironmentVariables.CI_STRING_TIME, nowAsTimeExpectedIsoString);
 
 
   // TODO what if it's a tag?
@@ -48,7 +52,6 @@ async function run(): Promise<void> {
   const pullRequest = event?.pull_request;
   setVariable(EnvironmentVariables.CI_PULL_REQUEST,  pullRequest?.number?.toString() ?? '');
   setVariable(EnvironmentVariables.CI_PR_NUMBER,  pullRequest?.html_url ?? 'false');
-
 }
 
 function setVariable(name: EnvironmentVariables, value: string) {
